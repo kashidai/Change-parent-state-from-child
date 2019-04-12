5fcda5e6593d8e15bfe0e8587793674e7eacfb8b
Dai <d.k.95.82.8192@icloud.com>
2019-04-12 14:14:35 -0700

    first add

---
+++ src/parent-child/Child.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  dataHoge: PropTypes.func,
+};
+
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  clickButton() {
+    return this.props.dataHoge();
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => { this.clickButton(); }}>ボタン</button>
+      </div>
+    );
+  }
+}
+
+Child.propTypes = propTypes;
+export default Child;
---
+++ src/parent-child/Parent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Child from "../parent-child/Child"
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hoge: 'hogehoge'
+    };
+  }
+
+  hogeFunc() {
+    this.setState({ hoge: '変えたぞ' });
+console.log(this.state.hoge);
+  }
+  render() {
+    return (
+      <div>
+        <Child dataHoge={() => { this.hogeFunc(); }} />
+      </div>
+    );
+  }
+}
+
+export default Parent;
